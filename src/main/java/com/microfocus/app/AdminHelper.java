4e0195ad5edd59970d8aff2853e97e0965377714
akevinlee <kevin.lee@microfocus.com>
2021-03-09 14:05:39 +0000

    Fixing AdminHelper - defect #2092

--- src/main/java/com/microfocus/app/AdminHelper.java
+++ src/main/java/com/microfocus/app/AdminHelper.java
@@ -5,9 +5,6 @@ import org.slf4j.LoggerFactory;
 public class AdminHelper {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(EmailEightBallApp.class);
 
-    private static String ADMIN_USER = "admin";
-    private static String ADMIN_PASSWORD = "password";
-
     AdminHelper() {
 
     }
