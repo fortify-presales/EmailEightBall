2276c03ac75333939a018bfe8efdef7b27cc6543
akevinlee <kevin.lee@microfocus.com>
2021-03-10 14:32:22 +0000

    Removing from demo

--- src/main/java/com/microfocus/app/AdminHelper.java
+++ src/main/java/com/microfocus/app/AdminHelper.java
@@ -5,9 +5,6 @@ import org.slf4j.LoggerFactory;
 public class AdminHelper {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(EmailEightBallApp.class);
 
-    private static String DEFAULT_USERNAME = "admin";
-    private static String DEFAULT_PASSWORD = "password";
-
     AdminHelper() {
 
     }
